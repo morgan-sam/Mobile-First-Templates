41e40e1a4e05d4953e88354b0d9ec254cc19cd07
Sam <samuel_morgan_@outlook.com>
2020-08-14 14:22:56 +0100

    Gets dominant color

--- website_01/src/App.jsx
+++ website_01/src/App.jsx
@@ -2,22 +2,29 @@ import React, { useState, useEffect } from 'react';
 import ColorThief from '../node_modules/colorthief/dist/color-thief.mjs';
 
 const App = () => {
+	const colorThief = new ColorThief();
 	const [ img, setImg ] = useState(null);
+	const [ dominantColor, setDominantColor ] = useState(null);
 
 	useEffect(() => {
 		const getImgColor = async () => {
-			// const colorThief = new ColorThief();
-			// const color = await colorThief.getColor(img);
-			// console.log(color);
-
 			const res = await fetch('https://source.unsplash.com/random');
 			const data = await res.blob();
-			const outside = URL.createObjectURL(data);
-			setImg(outside);
+			const pic = URL.createObjectURL(data);
+			var image = new Image();
+			image.src = pic;
+			image.onload = async () => {
+				const color = await colorThief.getColor(image);
+				setDominantColor(color);
+				setImg(pic);
+			};
 		};
 
 		getImgColor();
 	}, []);
+
+	console.log(dominantColor);
+
 	return (
 		<div className="app">
 			<div className="main-title-container">
