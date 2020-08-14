2ca473a22caf2ab863068fdf2c89e9cfd6e144bf
Sam <samuel_morgan_@outlook.com>
2020-08-14 19:22:03 +0100

    section-two classes

--- website_01/src/App.jsx
+++ website_01/src/App.jsx
@@ -8,6 +8,11 @@ const App = () => {
 					<h1 className="main-title">Lorem ipsum dolor sit amet.</h1>
 				</div>
 			</div>
+			<div className="section section-two">
+				<div className="section-two-title-container">
+					<h1 className="section-two-title">Lorem ipsum dolor sit amet.</h1>
+				</div>
+			</div>
 			<div className="section">
 				<div className="main-title-container">
 					<h1 className="main-title">Lorem ipsum dolor sit amet.</h1>
