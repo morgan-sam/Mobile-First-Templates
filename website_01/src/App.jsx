88e97cd27c6570ca3637fd6c1841968441fc8c05
Sam <samuel_morgan_@outlook.com>
2020-08-14 11:50:58 +0100

    Added basic title

---
+++ website_01/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const App = () => {
+	return (
+		<div className="app">
+			<div className="main-title-container">
+				<h1 className="main-title">Lorem ipsum dolor sit amet.</h1>
+			</div>
+		</div>
+	);
+};
+
+export default App;
