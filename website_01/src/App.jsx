c5120a9b925f8f97b6ec019ecd61fc255e5bbfd0
Sam <samuel_morgan_@outlook.com>
2020-08-14 19:17:39 +0100

    Renamed class to section

--- website_01/src/App.jsx
+++ website_01/src/App.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 
 const App = () => {
 	return (
-		<div className="app">
+		<div className="section">
 			<div className="main-title-container">
 				<h1 className="main-title">Lorem ipsum dolor sit amet.</h1>
 			</div>
