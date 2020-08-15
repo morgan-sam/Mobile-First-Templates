7ca40e6ba8321cd3b73dcbb8c5a5812c7c0036be
Sam <samuel_morgan_@outlook.com>
2020-08-15 17:16:17 +0100

    red/blue tunnels

--- website_01/src/components/TunnelBackground.jsx
+++ website_01/src/components/TunnelBackground.jsx
@@ -2,15 +2,27 @@ import React, { useState, useEffect } from 'react';
 
 const TunnelBackground = () => {
 	return (
-		<div className="tunnel-container">
-			{new Array(20).fill().map((el, i) => {
-				const curStyle = {
-					transform: `rotate(40deg) translateZ(${i * -20}px)`,
-					opacity: `${1 - 0.05 * i}`,
-					animationDelay: `${0.1 * i}s`
-				};
-				return <div key={i} style={curStyle} className="tunnel-box" />;
-			})}
+		<div className="tunnel-parent">
+			<div className="tunnel-container">
+				{new Array(20).fill().map((el, i) => {
+					const curStyle = {
+						transform: `rotate(40deg) translateZ(${i * -20}px)`,
+						opacity: `${1 - 0.05 * i}`,
+						animationDelay: `${0.1 * i}s`
+					};
+					return <div key={i} style={curStyle} className="tunnel-box" />;
+				})}
+			</div>
+			<div className="tunnel-container">
+				{new Array(20).fill().map((el, i) => {
+					const curStyle = {
+						transform: `rotate(40deg) translateZ(${i * -20}px)`,
+						opacity: `${1 - 0.05 * i}`,
+						animationDelay: `${0.1 * i}s`
+					};
+					return <div key={i} style={curStyle} className="tunnel-box" />;
+				})}
+			</div>
 		</div>
 	);
 };
