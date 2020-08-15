f64fae49da0538a444b5ae48f1ca25b396736f99
Sam <samuel_morgan_@outlook.com>
2020-08-15 17:07:03 +0100

    Alternating tunnel entrance

--- website_01/src/components/TunnelBackground.jsx
+++ website_01/src/components/TunnelBackground.jsx
@@ -3,10 +3,10 @@ import React, { useState, useEffect } from 'react';
 const TunnelBackground = () => {
 	return (
 		<div className="tunnel-container">
-			{new Array(10).fill().map((el, i) => {
+			{new Array(20).fill().map((el, i) => {
 				const curStyle = {
-					transform: `rotate(40deg) translateY(${i * 25}px) translateZ(${i * -10}px)`,
-					opacity: `${1 - 0.1 * i}`,
+					transform: `rotate(40deg) translateZ(${i * -20}px)`,
+					opacity: `${1 - 0.05 * i}`,
 					animationDelay: `${0.1 * i}s`
 				};
 				return <div key={i} style={curStyle} className="tunnel-box" />;
