65e6867409d7fedbc47ac0c8822a967a1eee9072
Sam <samuel_morgan_@outlook.com>
2020-08-18 15:50:31 +0100

    section2.css

--- website_01/src/index.js
+++ website_01/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App.jsx';
 import './css/reset.css';
 import './css/app.css';
+import './css/sections/section2.css';
 import './css/tunnelBackground.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
