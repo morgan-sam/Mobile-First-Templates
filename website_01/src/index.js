d27a857f7f9aa90a143b698682c91fbeb863fb70
Sam <samuel_morgan_@outlook.com>
2020-08-18 15:58:49 +0100

    Fixed sec 2 mix blend mode

--- website_01/src/index.js
+++ website_01/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App.jsx';
 import './css/reset.css';
 import './css/app.css';
-import './css/sections/section2.css';
 import './css/tunnelBackground.css';
+import './css/sections/section2.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
