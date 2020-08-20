a74434facf23b1b1dd49d75bd172142ae8e97f44
Sam <samuel_morgan_@outlook.com>
2020-08-20 14:02:46 +0100

    Added button to signup form

--- website_01/src/components/SignUpForm.jsx
+++ website_01/src/components/SignUpForm.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import Form from 'components/Form.jsx';
+import Button from '@material-ui/core/Button';
+import Box from '@material-ui/core/Box';
 
 const SignUpForm = () => {
 	return (
 		<div className="sign-up-form">
 			<Form label={'Email address'} helperText={'Please enter your email address'} />
 			<Form label={'Password'} helperText={'Please enter a password'} />
+			<Box p={5} display={'flex'} justifyContent="center">
+				<Button variant="contained" color="primary">
+					Sign Up
+				</Button>
+			</Box>
 		</div>
 	);
 };
